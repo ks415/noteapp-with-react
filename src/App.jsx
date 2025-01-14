1263237f1c60395c3f04d061e87a70038bece582
ks <146908390+ks415@users.noreply.github.com>
2025-01-14 15:44:20 +0900

    ローカルストレージに保存

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ import Sidebar from "./components/Sidebar";
 import uuid from "react-uuid";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
   const [activeNote, setActiveNote] = useState(null);
+
+  useEffect(() => {
+    //ローカルストレージにノードを保存する
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   const onAddNote = () => {
     console.log("追加");
