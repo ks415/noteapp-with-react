d0528e47c31977d911d45ac57b1338eca90fbd19
ks <146908390+ks415@users.noreply.github.com>
2025-01-14 13:48:18 +0900

    追加ボタンの実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css'
-import Main from './components/Main'
-import Sidebar from './components/Sidebar'
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("追加");
+
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "これは新しいノートです.",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
   return (
-    <div className='App'>
-      <Sidebar />
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
